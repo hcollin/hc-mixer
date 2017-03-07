b37a8f7678f2c796cf05184eef2d7d7e2947035f
Henrik Collin <hcollin78@gmail.com>
2017-03-07 10:34:09 +0200

    Initial commit with project config

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Services
+import '../styles/index.scss';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+
+      <div className="root-container">
+        <h1>Tracker</h1>
+      </div>
+    )
+  }
+}
